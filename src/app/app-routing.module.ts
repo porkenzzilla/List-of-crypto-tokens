158ccd4491453c221a69bad822d0e19f45a5d345
Olha Hlushko <olhahlushko@MacBook-Air-Olha.local>
2022-03-23 19:57:09 +0200

    add routing and some content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TokensComponent } from './tokens/tokens.component';
+import { NewsComponent } from './news/news.component';
+import { ElseComponent } from './else/else.component';
+
+const routes: Routes = [
+  {path: "", component: TokensComponent},
+  {path: "news", component: NewsComponent},
+  {path: "else", component: ElseComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/else/else.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-else',
+  templateUrl: './else.component.html',
+  styleUrls: ['./else.component.scss']
+})
+export class ElseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
