92e0ddfa4fd60db044b20821e6b2f6aaaa364371
Olha Hlushko <olhahlushko@MacBook-Air-Olha.local>
2022-03-24 12:40:49 +0200

    add content from http service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [HttpService]
 })
 export class AppComponent {
   title = 'List-of-crypto-tokens';
+  currentData: any;
+
+  constructor(private httpService: HttpService){}
+
+  ngOnInit(){
+    this.httpService.getData().subscribe((data: any) => {
+      this.currentData = data.tokens
+      console.log(data.tokens) })
+  }
+  
 }
