c2a714e9caa4810aec05f5a6378ba823501ae29a
Olha Hlushko <olhahlushko@MacBook-Air-Olha.local>
2022-03-26 12:38:02 +0200

    add all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../http.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [HttpService]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'List-of-crypto-tokens';
-  currentData: any;
-
-  constructor(private httpService: HttpService){}
 
   ngOnInit(){
-    this.httpService.getData().subscribe((data: any) => {
-      this.currentData = data.tokens
-      console.log(data.tokens) })
   }
   
 }
--- src/app/tokens/tokens.component.ts
+++ src/app/tokens/tokens.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../http.service';
 
 @Component({
   selector: 'app-tokens',
   templateUrl: './tokens.component.html',
-  styleUrls: ['./tokens.component.scss']
+  styleUrls: ['./tokens.component.scss'],
+  providers: [HttpService]
 })
 export class TokensComponent implements OnInit {
-
-  constructor() { }
+  currentData: any;
+  
+  constructor(private httpService: HttpService){}
 
   ngOnInit(): void {
+    this.httpService.getData().subscribe((data: any) => {
+      this.currentData = data.tokens
+      console.log(data.tokens) })
   }
 
 }
