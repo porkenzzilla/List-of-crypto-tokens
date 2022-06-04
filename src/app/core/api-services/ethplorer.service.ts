603dc6755939aeac02b1d0d14be6ea8237e1d2dd
porkenzzilla <admin@admins-MacBook-Air.local>
2022-06-04 18:57:28 +0300

    improve project

--- src/app/core/api-services/ethplorer.service.ts
+++ src/app/core/api-services/ethplorer.service.ts
@@ -3,11 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {IEthplorerResponse, Token} from '../interfaces/ethplorer.interface';
-import {environment} from 'src/environments/environment.prod';
-
-// const API_URL = "api.ethplorer.io"; // @todo move to environments
-// запитай чи в продакшен чи в розробку, я закинув в продакшен
-// при зборці проекту вміст зміниться з dev на prod?
+import {environment} from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
